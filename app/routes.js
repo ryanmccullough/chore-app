4fa03838085225040acc7335e321b7d6681e2574
Ryan McCullough <ryan@iMac.local>
2014-11-04 13:46:16 -0600

    Test routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,61 @@
 // routes
 
 module.exports = function(app, passport, Chore) {
+
+// USER ROUTES
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/api/user', function(req, res) {
+        res.sendfile('./public/user.html'); // load the index.ejs file
+    });
+
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/user/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') }); 
+    });
+
+    // process the login form
+    // app.post('/login', do all our passport stuff here);
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    // app.post('/signup', do all our passport stuff here);
+
+    // =====================================
+    // PROFILE SECTION =====================
+    // =====================================
+    // we will want this protected so you have to be logged in to visit
+    // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+    // =====================================
+    // LOGOUT ==============================
+    // =====================================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+// CHORES ROUTES
 app.get('/api/chores',function(req, res) {
 	Chore.find(function(err, chores) {
 		if (err)
@@ -36,7 +91,7 @@ app.delete('/api/chores/:chore_id', function(req, res) {
 	});
 });
 // angular call
-app.get('*', function(req, res){
+app.get('/api/chores*', function(req, res){
 	res.sendfile('./public/index.html');
 });
 };
